71650c85cfdece19e1a2a8b7e2039ef2c642df66
MattBegbie <22484516+MattBegbie@users.noreply.github.com>
2023-03-04 08:29:44 -0500

    small changes, using querySelectorAll()

--- scripts/content.js
+++ scripts/content.js
@@ -1,3 +1,18 @@
-const article = document.querySelector("article");
+// const content = document.querySelectorAll("p");
 
-console.log(article);
+// console.log(content);
+// for (const key in content) {
+		// console.log(content[key]);
+		// const text = "<b>"+content[key]+"</b>";
+		// const text = "<b>"+content+"</b>";
+		const old = document.querySelectorAll("p");
+for (let index = 0; index < document.querySelectorAll("p").length; index++) {
+	// const element = array[index];
+	console.log(old[index], " Index is : ", index);
+	const newText = "<b>"+old[index]+"</b>";
+	// console.log(newText, " new Index is : ", index);
+	// document.querySelectorAll("p")[index].innerHTML = "<b>"+document.querySelectorAll("p")[index]+"</b>";
+	
+}
+// }
+
