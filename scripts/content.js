e9ba1f1e889e9ede46e6c80a7ea7a3633dd21226
MattBegbie <22484516+MattBegbie@users.noreply.github.com>
2023-03-04 08:39:12 -0500

    efficiency change

--- scripts/content.js
+++ scripts/content.js
@@ -1,5 +1,6 @@
 const old = document.querySelectorAll("p");
-for (let index = 0; index < document.querySelectorAll("p").length; index++) {
+
+for (let index = 0; index < old.length; index++) {
   console.log(old[index], " Index is : ", index);
   const newText = "".concat("<b>", old[index].textContent, "</b>");
   console.log(newText);
