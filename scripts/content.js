9c0931c9d7ef30d963a9871ef783e534f35a74a8
MattBegbie <22484516+MattBegbie@users.noreply.github.com>
2023-03-04 08:38:06 -0500

    got text editing working, however everything is bold

--- scripts/content.js
+++ scripts/content.js
@@ -1,18 +1,7 @@
-// const content = document.querySelectorAll("p");
-
-// console.log(content);
-// for (const key in content) {
-		// console.log(content[key]);
-		// const text = "<b>"+content[key]+"</b>";
-		// const text = "<b>"+content+"</b>";
-		const old = document.querySelectorAll("p");
+const old = document.querySelectorAll("p");
 for (let index = 0; index < document.querySelectorAll("p").length; index++) {
-	// const element = array[index];
-	console.log(old[index], " Index is : ", index);
-	const newText = "<b>"+old[index]+"</b>";
-	// console.log(newText, " new Index is : ", index);
-	// document.querySelectorAll("p")[index].innerHTML = "<b>"+document.querySelectorAll("p")[index]+"</b>";
-	
+  console.log(old[index], " Index is : ", index);
+  const newText = "".concat("<b>", old[index].textContent, "</b>");
+  console.log(newText);
+  document.querySelectorAll("p")[index].innerHTML = newText;
 }
-// }
-
