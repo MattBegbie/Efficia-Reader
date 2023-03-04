4b13afa1d62863615161bd221bc02d9d3c301393
MattBegbie <22484516+MattBegbie@users.noreply.github.com>
2023-03-04 09:15:03 -0500

    added todo in readme added created new function to boldenate words - incomplete

---
+++ readme.md
@@ -0,0 +1,14 @@
+NonWorking Websites
+
+- Reddit
+
+Working Websites
+
+- Wikipedia
+- w3schools
+
+TODO:
+
+- add prototype for string
+- add closing bold
+- return words back into paragraphs
--- scripts/content.js
+++ scripts/content.js
@@ -1,8 +1,29 @@
-const old = document.querySelectorAll("p");
-
+let old = document.querySelectorAll("p");
+// function boldenate();
+// const word= {"", ""};
 for (let index = 0; index < old.length; index++) {
   console.log(old[index], " Index is : ", index);
-  const newText = "".concat("<b>", old[index].textContent, "</b>");
+
+  newText = boldenate(old[index].textContent);
   console.log(newText);
   document.querySelectorAll("p")[index].innerHTML = newText;
 }
+
+function boldenate(paragraph) {
+
+  const words = paragraph.split(" ");
+  for (let l = 0; l < words.length; l++) {
+	let word = words[l]
+	let midP = Math.round(word.length/3);
+	console.log(midP);
+	
+	// word = "".concat("<b>", word)
+	// word = word.length/2;
+
+
+	// let word = "".concat("<b>", words[l].textContent, "</b>");
+    // console.log(word[l]);
+  }
+  return words;
+}
+
