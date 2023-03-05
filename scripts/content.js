78547493bcd8126d98035e2206b936d281610b7b
MattBegbie <22484516+MattBegbie@users.noreply.github.com>
2023-03-04 21:32:13 -0500

    it works!!

--- scripts/content.js
+++ scripts/content.js
@@ -1,29 +1,17 @@
 let old = document.querySelectorAll("p");
-// function boldenate();
-// const word= {"", ""};
 for (let index = 0; index < old.length; index++) {
-  console.log(old[index], " Index is : ", index);
-
   newText = boldenate(old[index].textContent);
-  console.log(newText);
   document.querySelectorAll("p")[index].innerHTML = newText;
 }
 
 function boldenate(paragraph) {
-
   const words = paragraph.split(" ");
+  let newWords = "";
   for (let l = 0; l < words.length; l++) {
-	let word = words[l]
-	let midP = Math.round(word.length/3);
-	console.log(midP);
-	
-	// word = "".concat("<b>", word)
-	// word = word.length/2;
-
-
-	// let word = "".concat("<b>", words[l].textContent, "</b>");
-    // console.log(word[l]);
+    let word = words[l];
+    let point = Math.round(word.length / 3);
+    word = "<b>" + word.slice(0, point) + "</b>" + word.slice(point);
+    newWords = newWords + " " + word;
   }
-  return words;
+  return newWords;
 }
-
